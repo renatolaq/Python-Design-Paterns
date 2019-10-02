d63d8fc8007c7842ce0256740314d090a265fc01
Renato Pereira <renato.laq@gmail.com>
2019-10-02 16:48:34 -0300

    inserction new process

--- testa_conta.py
+++ testa_conta.py
@@ -61,6 +61,50 @@ class ContaBanco:
                 print("")
                 print("")
 
+    def conta_extrato(self):
+        print("===================================================")
+        print(" BANCO ACME - EXTRATO CONTA  " + self.data_atual())
+        print("=================================================== ")
+        print("")
+        selecao = input("INFORME SUA CONTA ")
+        chave = 0
+        for info in self.contas:
+            agencia = info['agencia']
+            conta = info['conta']
+            nome = info['nome']
+            saldo = info['saldo']
+            limite = info['limite']
+            transacao = info['transacao']
+            if selecao == conta:
+                print("")
+                print("")
+                print("===================================================")
+                print("BANCO ACME - DEPOSITO EM CONTA - " + self.data_atual())
+                print("=================================================== ")
+                print("AGENCIA " + agencia + "/" + conta)
+                print(nome.upper())
+                print("=================================================== ")
+                print("            EXTRATO DE MOVIMENTAÇÕES")
+                print("=================================================== ")
+                if len(transacao) == 0:
+                    print("  *** NÃO EXISTE MOVIMENTAÇÕES NA CONTA ***")
+                else:
+                    for registro in transacao:
+                        print(registro['data'] + " " + registro['operacao'] + " " + "{:.2f} ".format(registro['valor']))
+                print("")
+                print("=================================================== ")
+                print("SALDO DISPONIVEL R$ " + "{:.2f} ".format(saldo))
+                print("LIMITE CREDITO   R$ " + "{:.2f} ".format(limite))
+                print("SALDO TOTAL      R$ " + "{:.2f} ".format(saldo + limite))
+
+                print("=================================================== ")
+                print("")
+                input("PRESSIONE < ENTER > PARA CONTINUAR")
+                print("")
+                print("")
+                return
+            chave += 1
+        input("CONTA NAO IDENTIFICADA < ENTER > PARA CONTINUAR")
 
     def conta_deposito(self):
         print("===================================================")
@@ -151,7 +195,7 @@ class ContaBanco:
                     self.contas[chave]['limite'] = sa['limite']
                     self.contas[chave]['saldo_total'] = sa['saldo'] + sa['limite']
                     # Gravar a Transacao
-                    registro = Transacao.gera_transacao(self, self.data_atual(), "(-) SAQUE ", valor_saque)
+                    registro = Transacao.gera_transacao(self, self.data_atual(), "(-) SAQUE    ", valor_saque)
                     self.contas[chave]['transacao'].append(registro)
                     a = 0
 
@@ -182,21 +226,25 @@ class ContaBanco:
             limite = limite - diferenca
         return {'saldo':saldo, 'limite':limite}
 
-    def conta_extrato(self):
-        return
 
     def gerar_contas(self):
-        conta1 = {'agencia': '0001', 'conta':'00001-1', 'nome':'Renato Pereira', 'saldo': 1000.0, 'limite':0.0, 'saldo_total': 1000.0, 'data_abertura':'02/10/2019 10:41:10', 'transacao':[]}
-        conta2 = {'agencia': '0001', 'conta':'00002-2', 'nome': 'Aline Blasco', 'saldo': 1000.0,  'limite':0.0, 'saldo_total': 1000.0, 'data_abertura':'02/10/2019 10:41:10', 'transacao':[]}
+        conta1 = {'agencia': '0001', 'conta':'00001-1', 'nome':'Renato Pereira', 'cpf': '11707899860', 'saldo': 1000.0, 'limite':0.0, 'saldo_total': 1000.0, 'data_abertura':'02/10/2019 10:41:10', 'transacao':[], 'cadastro':[{'cpf':'11707899860', 'rg':'21967629', 'telefone':'1146395855'}]}
+        conta2 = {'agencia': '0001', 'conta':'00002-2', 'nome': 'Aline Blasco',  'cpf': '43061665897','saldo': 1000.0,  'limite':0.0, 'saldo_total': 1000.0, 'data_abertura':'02/10/2019 10:41:10', 'transacao':[], 'cadastro':[{'cpf':'43061668597', 'rg':'21963222', 'telefone':'1146395855'}]}
         self.contas.append(conta1)
         self.contas.append(conta2)
         return self.contas
 
     def conta_lista(self):
         print("===================================================")
-        print("BANCO ACME -   CONTAS ATIVAS    -  "+self.data_atual())
+        print("BANCO ACME - CONTAS ATIVAS -  "+self.data_atual())
         print("=================================================== ")
         print("")
+        for conta in self.contas:
+            print(conta['agencia'] +"/"+conta['conta']+" CPF "+conta['cpf']+"  "+conta['nome'])
+
+        print("")
+        input("PRESSIONE < ENTER > PARA CONTINUAR")
+
 
     def conta_abertura(self):
         print("")
@@ -208,46 +256,51 @@ class ContaBanco:
         input("PRESSIONE < ENTER > PARA CONTINUAR")
 
 
+if __name__ == '__main__':
 
 
-c = ContaBanco()
-con = c.gerar_contas()
-acao = True
-while acao == True:
-    print("===================================================")
-    print("BANCO ACME - ABC               "+c.data_atual())
-    print("=================================================== ")
-    print("                 MENU CLIENTE")
-    print("=================================================== ")
-    print("         0 - VERFICAÇÃO SALDO                          ")
-    print("         1 - DEPOSITOS                                 ")
-    print("         2 - SAQUE                                     ")
-    print("         3 - TRANSFERENCIAS                            ")
-    print("         4 - EXTRATO CONTA")
-    print("=================================================== ")
-    print("               MENU ADMINISTRATIVO")
-    print("=================================================== ")
-    print("         5 - LISTAGEM DE CONTAS ATIVAS                 ")
-    print("         6 - ABERTURA DE CONTAS")
-    print("         7 - LIBERA LIMITE CREDITO")
-    print("         9 - SAIR DO SISTEMA                           ")
-    print("===================================================")
-    escolha = input("OPÇÃO DESEJADA ")
-    if escolha == '':
-        continue
+    c = ContaBanco()
+    con = c.gerar_contas()
+    acao = True
+    while acao == True:
+        print("===================================================")
+        print("BANCO ACME - ABC               "+c.data_atual())
+        print("=================================================== ")
+        print("                 MENU CLIENTE")
+        print("=================================================== ")
+        print("         0 - VERIFICAÇÃO SALDO                          ")
+        print("         1 - DEPOSITOS                                 ")
+        print("         2 - SAQUE                                     ")
+        print("         3 - TRANSFERENCIAS                            ")
+        print("         4 - EXTRATO CONTA")
+        print("=================================================== ")
+        print("               MENU ADMINISTRATIVO")
+        print("=================================================== ")
+        print("         5 - LISTAGEM DE CONTAS ATIVAS                 ")
+        print("         6 - ABERTURA DE CONTAS")
+        print("         7 - LIBERA LIMITE CREDITO")
+        print("         8 - DADOS CADASTRAIS CLIENTE")
+        print("         99 - SAIR DO SISTEMA                           ")
+        print("===================================================")
+        escolha = input("OPÇÃO DESEJADA ")
+        print("")
+        if escolha == '':
+            continue
 
-    elif int(escolha) == 0:
-        c.conta_saldo()
-    elif int(escolha) == 1:
-        c.conta_deposito()
-    elif int(escolha) == 2:
-        c.conta_saque()
-    elif int(escolha) == 5:
-        c.conta_lista()
-    elif int(escolha) == 6:
-        c.conta_abertura()
-    elif int(escolha) == 9:
-        acao = False
+        elif int(escolha) == 0:
+            c.conta_saldo()
+        elif int(escolha) == 1:
+            c.conta_deposito()
+        elif int(escolha) == 2:
+            c.conta_saque()
+        elif int(escolha) == 4:
+            c.conta_extrato()
+        elif int(escolha) == 5:
+            c.conta_lista()
+        elif int(escolha) == 6:
+            c.conta_abertura()
+        elif int(escolha) == 99:
+            acao = False
 
 
 
